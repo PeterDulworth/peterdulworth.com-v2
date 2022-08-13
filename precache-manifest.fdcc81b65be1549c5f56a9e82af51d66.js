100478f9cc564fe996a35451879e0f9792aaa599
Peter Dulworth <pdulworth97@gmail.com>
2022-08-13 00:13:55 -0700

    Updates

---
+++ precache-manifest.fdcc81b65be1549c5f56a9e82af51d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "665dcea57032f3c16f0a",
+    "url": "/peterdulworth.com-v2/static/css/main.4f012db7.chunk.css"
+  },
+  {
+    "revision": "665dcea57032f3c16f0a",
+    "url": "/peterdulworth.com-v2/static/js/main.cf5fc9b2.chunk.js"
+  },
+  {
+    "revision": "d67861d52a9f66113422",
+    "url": "/peterdulworth.com-v2/static/js/runtime~main.4ed6d055.js"
+  },
+  {
+    "revision": "dc68128106613bf95eb7",
+    "url": "/peterdulworth.com-v2/static/js/2.8973162e.chunk.js"
+  },
+  {
+    "revision": "393df4dd135c131d23f2ce60e6522636",
+    "url": "/peterdulworth.com-v2/static/media/Objective-Bold.393df4dd.otf"
+  },
+  {
+    "revision": "dfd73d4c6e323e837df3aa8460751f3b",
+    "url": "/peterdulworth.com-v2/static/media/Objective-Regular.dfd73d4c.otf"
+  },
+  {
+    "revision": "7d292ff30556562e17f30c3017e692e8",
+    "url": "/peterdulworth.com-v2/index.html"
+  }
+];
